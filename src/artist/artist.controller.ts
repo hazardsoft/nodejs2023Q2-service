63a7c98cc1e0e782c4e62e78da3d720771fdb002
Henadzi Shutko <hazardsoft@gmail.com>
2023-07-27 14:29:53 +0300

    feat: implement "/artist" endpoint fully
    details: test artists.e2e.spec.ts fails as "/album" endpoint is not implemented yet

--- src/artist/artist.controller.ts
+++ src/artist/artist.controller.ts
@@ -1,34 +1,65 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  ParseUUIDPipe,
+  HttpCode,
+  Put,
+} from '@nestjs/common';
 import { ArtistService } from './artist.service';
 import { CreateArtistDto } from './dto/create-artist.dto';
-import { UpdateArtistDto } from './dto/update-artist.dto';
+import { Artist } from './entities/artist.entity';
+import { TrackService } from 'src/track/track.service';
+import { CreateTrackDto } from 'src/track/dto/create-track.dto';
 
 @Controller('artist')
 export class ArtistController {
-  constructor(private readonly artistService: ArtistService) {}
+  constructor(
+    private readonly artistService: ArtistService,
+    private readonly trackService: TrackService,
+  ) {}
 
   @Post()
-  create(@Body() createArtistDto: CreateArtistDto) {
-    return this.artistService.create(createArtistDto);
+  async create(@Body() createTrackDto: CreateArtistDto): Promise<Artist> {
+    return this.artistService.create(createTrackDto);
   }
 
   @Get()
-  findAll() {
+  async findAll(): Promise<Artist[]> {
     return this.artistService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.artistService.findOne(+id);
+  async findOne(@Param('id', ParseUUIDPipe) id: string): Promise<Artist> {
+    return this.artistService.findOne(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateArtistDto: UpdateArtistDto) {
-    return this.artistService.update(+id, updateArtistDto);
+  @Put(':id')
+  async update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateArtistDto: CreateArtistDto,
+  ): Promise<Artist> {
+    return this.artistService.update(id, updateArtistDto);
   }
 
+  @HttpCode(204)
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.artistService.remove(+id);
+  async remove(@Param('id', ParseUUIDPipe) id: string): Promise<void> {
+    const removedArtist = await this.artistService.remove(id);
+    if (removedArtist) {
+      const tracks = await this.trackService.findAll();
+      const tracksToUpdate = tracks.filter(
+        (t) => t.artistId === removedArtist.id,
+      );
+      tracksToUpdate.forEach((t) => {
+        const trackUpdateDto = Object.assign({ ...t }, <
+          Partial<CreateTrackDto>
+        >{ artistId: null });
+        this.trackService.update(t.id, trackUpdateDto);
+      });
+    }
   }
 }
--- src/artist/artist.service.ts
+++ src/artist/artist.service.ts
@@ -1,26 +1,53 @@
 import { Injectable } from '@nestjs/common';
+import { v4 as uuidv4 } from 'uuid';
 import { CreateArtistDto } from './dto/create-artist.dto';
-import { UpdateArtistDto } from './dto/update-artist.dto';
+import { Artist } from './entities/artist.entity';
+import { ArtistNotFoundError } from './errors';
 
 @Injectable()
 export class ArtistService {
-  create(createArtistDto: CreateArtistDto) {
-    return 'This action adds a new artist';
+  private readonly artists: Artist[] = [];
+
+  async create(createArtistDto: CreateArtistDto): Promise<Artist> {
+    const artist = new Artist();
+    artist.id = uuidv4();
+    Object.assign(artist, createArtistDto);
+
+    this.artists.push(artist);
+    return artist;
   }
 
-  findAll() {
-    return `This action returns all artist`;
+  async findAll(): Promise<Artist[]> {
+    return this.artists.slice();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} artist`;
+  async findOne(id: string): Promise<Artist | undefined> {
+    const artist = this.artists.find((a) => a.id === id);
+    if (!artist) {
+      throw new ArtistNotFoundError(id);
+    }
+    return artist;
   }
 
-  update(id: number, updateArtistDto: UpdateArtistDto) {
-    return `This action updates a #${id} artist`;
+  async update(
+    id: string,
+    updateArtistDto: CreateArtistDto,
+  ): Promise<Artist | undefined> {
+    const artist = this.artists.find((a) => a.id === id);
+    if (!artist) {
+      throw new ArtistNotFoundError(id);
+    }
+    Object.assign(artist, updateArtistDto);
+    return artist;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} artist`;
+  async remove(id: string): Promise<Artist | undefined> {
+    const foundIndex = this.artists.findIndex((u) => u.id === id);
+    if (foundIndex === -1) {
+      throw new ArtistNotFoundError(id);
+    }
+    const artist = this.artists.splice(foundIndex, 1).shift();
+
+    return artist;
   }
 }
