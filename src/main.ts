bfcb6b50ad59c0b7224bd77015467ede595fa42c
Henadzi Shutko <hazardsoft@gmail.com>
2023-07-28 11:30:47 +0300

    feat: add swagger to generate open api 3.0 docs

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT');
+
+  const config = new DocumentBuilder()
+    .setTitle('Home Library')
+    .setDescription('Home Library API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(
     new ValidationPipe({
