6ff312e6ba728c0e78c57e5e6efdc84f01c38978
Henadzi Shutko <hazardsoft@gmail.com>
2023-07-28 11:32:38 +0300

    feat: add api responses decorators in User controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -10,7 +10,9 @@ import {
   ParseUUIDPipe,
   UseInterceptors,
   ClassSerializerInterceptor,
+  HttpStatus,
 } from '@nestjs/common';
+import { ApiResponse } from '@nestjs/swagger';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdatePasswordDto } from './dto/update-password.dto';
@@ -22,6 +24,10 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: 'required body fields are not supplied',
+  })
   async create(@Body() createUserDto: CreateUserDto): Promise<User> {
     return this.userService.create(createUserDto);
   }
@@ -32,11 +38,31 @@ export class UserController {
   }
 
   @Get(':id')
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: 'supplied id is not UUID',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'user id is not found',
+  })
   async findOne(@Param('id', ParseUUIDPipe) id: string): Promise<User> {
     return this.userService.findOne(id);
   }
 
   @Put(':id')
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: 'supplied id is not UUID',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'user id is not found',
+  })
+  @ApiResponse({
+    status: HttpStatus.FORBIDDEN,
+    description: 'old password does not match',
+  })
   async update(
     @Param('id', ParseUUIDPipe) id: string,
     @Body() updatePasswordDto: UpdatePasswordDto,
@@ -46,6 +72,14 @@ export class UserController {
 
   @HttpCode(204)
   @Delete(':id')
+  @ApiResponse({
+    status: HttpStatus.NO_CONTENT,
+    description: 'user successfully deleted',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'user id is not found',
+  })
   async remove(@Param('id', ParseUUIDPipe) id: string): Promise<void> {
     await this.userService.remove(id);
   }
