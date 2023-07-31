cfd9636eb1ba54750295aaa4ddb54ce91639b4ac
Henadzi Shutko <hazardsoft@gmail.com>
2023-07-31 23:41:13 +0300

    fix: fallback port value to 4000 in case .env is absent

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
-  const port = configService.get<number>('PORT');
+  const port = configService.get<number>('PORT') || 4000;
 
   const config = new DocumentBuilder()
     .setTitle('Home Library Service')
