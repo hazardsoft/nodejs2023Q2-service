245cef20565dbccd999fe937d563da57a0b6b8b8
Henadzi Shutko <hazardsoft@gmail.com>
2023-07-31 17:11:49 +0300

    fix: correct server api url in swagger

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
     .setTitle('Home Library Service')
     .setDescription('Home music library service')
     .setVersion('1.0.0')
-    .addServer('/api')
+    .addServer('/')
     .build();
 
   const document = SwaggerModule.createDocument(app, config, {
